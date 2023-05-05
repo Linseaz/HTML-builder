840ac6abc426ec2e742cf7673dceadf8d18e8e12
linseaz <linsea@online.ua>
2023-05-05 15:08:20 +0300

    feat: finish task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+const { readdir } = require('fs/promises');
+
+const copyFilePath = path.join(__dirname, 'files');
+const pasteFilePath = path.join(__dirname, 'files-copy');
+
+async function copyFile() {
+  fs.mkdir(pasteFilePath, { recursive: true }, err => {
+    if (err) throw err;
+  });
+
+  const files = await readdir(copyFilePath, { withFileTypes: true });
+  files.forEach(file => {
+    fs.copyFile(path.join(copyFilePath, file.name), path.join(pasteFilePath, file.name), err => {
+      if (err) throw err;
+    });
+  });
+  console.log('Копирование завершено успешно.');
+}
+
+fs.access(pasteFilePath, fs.F_OK, (err) => {
+  if (err) {
+    copyFile();
+    return;
+  }
+
+  fs.rm(pasteFilePath, { recursive: true }, err => {
+    if (err) throw err;
+    else copyFile();
+  });
+});
+
+
 
